3aef13a64a2ab34d903eb1d540d475e672dde40c
yujiayu <yujiayu@lizhi.fm>
2019-03-01 10:59:15 +0800

    试一下只更新com3

--- packages/component3/src/index.ts
+++ packages/component3/src/index.ts
@@ -4,4 +4,4 @@ import {getName as getName2} from "test-lerna-lib2";
 console.log(`在com3调用${getName1()}`);
 console.log(`在com3调用${getName2()}`);
 
-console.log(`我是com3`);
+console.log(`我是com3 test1`);
